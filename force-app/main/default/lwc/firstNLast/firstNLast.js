61a49b61ae2967362f3198a9ca41f6af69eab69a
dubeyabhishek1005 <dubeyabhishek1005@gmail.com>
2026-02-05 03:38:43 +0530

    First & Last with Iterator:it

---
+++ force-app/main/default/lwc/firstNLast/firstNLast.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class FirstNLast extends LightningElement {
+
+    items = [
+        {id: 1, name: 'Abhishek'},
+        {id: 2, name: 'Rahul'},
+        {id: 3, name: 'Rajesh'},
+        {id: 4, name: 'Rakesh'},
+        {id: 5, name: 'Rajat'},
+        {id: 6, name: 'Rajesh'},
+        {id: 7, name: 'Akash'}
+    ];
+}
